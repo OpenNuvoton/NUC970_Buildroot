8134b446392e633dde84bbeea1fcedbd78403b71
Leven925 <95616341+Leven925@users.noreply.github.com>
2021-12-10 11:59:07 +0800

    Update install_rootfs_emwin.sh
    N9H30 can execute rcS when Linux boot up

--- board/nuvoton/install_rootfs_emwin.sh
+++ board/nuvoton/install_rootfs_emwin.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm output/target/etc/resolv.conf
+chmod 777 board/nuvoton/rootfs-emwin/etc/init.d/rcS
 cp -a board/nuvoton/rootfs-emwin/* output/target/
 cp -a output/build/applications-1.0.0/emWin/Sample/SimpleDemo/SimpleDemo output/target/usr/bin
 cp -a output/build/applications-1.0.0/emWin/Sample/GUIDemo/GUIDemo output/target/usr/bin
